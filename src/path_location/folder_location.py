0fa928f723c2ac09ca1030e05450085a010ac210
darryl-chan <darrylchan8@yahoo.com.sg>
2025-04-05 15:15:27 +0800

    add file for folder location

---
+++ src/path_location/folder_location.py
@@ -0,0 +1,21 @@
+import os
+
+#####################
+# Processed folder
+#####################
+PROCESSED_DATA_FOLDER = '../data_untracked/processed'
+ABNORMAL_CSV = 'merged_txns_SNORKEL.csv'
+FULL_FEATURES_FILE = 'full_features.csv'
+
+#####################
+# Features folder
+#####################
+FEATURES_DATA_FOLDER = '../data_untracked/features'
+MERGED_RELATIONSHIP_FILE = 'merged_relationships_full.csv'
+
+#####################
+# SEC folder
+#####################
+SEC_SUBMISSIONS_FOLDER = '../data_untracked/raw/sec_submissions'
+SEC_DATA_FOLDER = "../data_untracked/raw/sec_submissions/compiled"
+FOOTNOTE_FILE = "FOOTNOTES.csv"
